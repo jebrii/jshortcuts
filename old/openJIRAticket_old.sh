5e732f55ecaae47601be5a457a426eba244d9088
jebri <h3nryraschke@gmail.com>
2017-10-05 19:28:54 -0700

    flip flopping

--- old/openJIRAticket_old.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -eq 0 ]; then
-	echo "ERROR: Ticket number not provided."
-else
-	TicketNum=$1
-	echo "Opening ticket "$1
-	open -a "Google Chrome" 'https://candicontrols.atlassian.net/browse/CANDI-'$1
-fi
