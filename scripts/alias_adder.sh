a7652bb82715cd7213a608059828a66af4d3990c
Henry <12600324+jebrii@users.noreply.github.com>
2019-05-16 17:34:37 -0700

    Added rudimentary list feature to alias_adder

--- scripts/alias_adder.sh
+++ scripts/alias_adder.sh
@@ -17,7 +17,7 @@ if [ -n "$1" -a "${1:0:1}" != "-" ]; then
 	fi
 fi
 
-while getopts ":i:a:n:rh" opt; do
+while getopts ":i:a:n:rlh" opt; do
 	if [ ${OPTARG:0:1} = "-" ] 2>/dev/null ; then
 		echo -e "${RED}ERROR: Missing argument for $opt.${NC}" >&2
 		cat "$JSHOR/util/help_pages/alias_adder_help.txt"
@@ -32,6 +32,12 @@ while getopts ":i:a:n:rh" opt; do
     a) al_ip="$OPTARG";;
     n) al_nm="$OPTARG";;
     r) remove=true;;
+		l)
+			echo -n "Aliases currently set up with this tool:"
+			# Make this compare /tmp/aliases-list.txt to ifconfig?
+			cat "/tmp/aliases-list.txt"
+			exit 0
+			;;
 		:)
 			echo -e "${RED}ERROR: Missing argument for $OPTARG.${NC}" >&2
 			cat "$JSHOR/util/help_pages/alias_adder_help.txt"
@@ -45,17 +51,22 @@ while getopts ":i:a:n:rh" opt; do
 done
 
 if $remove; then
-  ifconfig $al_iface -alias $al_ip $al_nm
+  sudo ifconfig $al_iface -alias $al_ip $al_nm
   if [ $? -eq 0 ]; then
+		sed -e "s/$al_iface $al_ip $al_nm//g" -i .backup "/tmp/aliases-list.txt"
     echo "Alias successfully removed from $al_iface with IP of $al_ip and netmask of $al_nm"
   else
     echo -e "${RED}ERROR: Alias remove failed.${NC}"
     exit 1
   fi
 else
-  ifconfig $al_iface alias $al_ip $al_nm
+  sudo ifconfig $al_iface alias $al_ip $al_nm
   if [ $? -eq 0 ]; then
-    echo "Alias successfully added to $al_iface with IP of $al_ip and netmask of $al_nm"
+		# if [ ! -f "/tmp/aliases-list.txt"]; then
+		# 	touch "/tmp/aliases-list.txt"
+		# fi
+		echo "$al_iface $al_ip $al_nm" >> "/tmp/aliases-list.txt"
+		echo "Alias successfully added to $al_iface with IP of $al_ip and netmask of $al_nm"
   else
     echo -e "${RED}ERROR: Alias add failed.${NC}"
     exit 1
