ef634c47ed03a4e2085b5d4647efe7d6d52897f4
Henry <12600324+jebrii@users.noreply.github.com>
2019-06-10 12:00:52 -0700

    Removed sudo from aliases.sh

--- util/aliases.sh
+++ util/aliases.sh
@@ -6,7 +6,7 @@ alias dag='bash "$JSHOR/scripts/dag.sh"'
 alias jira='bash "$JSHOR/scripts/open_jira.sh"'
 alias conf='bash "$JSHOR/scripts/open_conf.sh"'
 alias peek='bash "$JSHOR/scripts/peek.sh"'
-alias ala='sudo bash "$JSHOR/scripts/alias_adder.sh"'
+alias ala='bash "$JSHOR/scripts/alias_adder.sh"'
 alias addkeys='bash "$JSHOR/scripts/add_keys.sh"'
 alias gmd='bash "$JSHOR/scripts/candi_meter_query.sh"'
 
